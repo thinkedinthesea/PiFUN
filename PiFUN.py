7d68955bbd89506140e051c6c048db214004429e
Devid <45665085+thinkedinthesea@users.noreply.github.com>
2019-11-08 14:16:46 +0100

    Update PiFUN.py

--- PiFUN.py
+++ PiFUN.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python
 #
+#THINKEDINTHESEA
+#PiFUN
+#
+#rev081119
+#
+import RPIO
+#import time
+import os
+#
+RPIO.cleanup()
+poweroff = 0
+#segnale di vitalità inviato
+RPIO.setup(23, RPIO.OUT)
+#segnale di vitalità ricevuto
+RPIO.setup(10, RPIO.IN)
+#start mettiamo su la vitalità
+RPIO.output(23, True)
+#
+#ciclo principale
+while True:
+  #se perdiamo la vitalità ricevuta spegniamo
+  if (RPIO.input(10) == False):
+      os.system("sudo poweroff")
+      break
+  #
+  time.sleep(0.1)
