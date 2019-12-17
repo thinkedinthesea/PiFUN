9add8d49ca6567d5f7317ed68d02484bfa364a3a
Devid <45665085+thinkedinthesea@users.noreply.github.com>
2019-12-17 12:54:13 +0100

    Update pyfun.py

--- pyfun.py
+++ pyfun.py
@@ -5,3 +5,18 @@
 #read file
 import configparser
 config = configparser.ConfigParser()
+config.read('pyfun.cfg')
+#assign GPIO
+LEFT = config['DEFAULT']['LEFT']
+RIGHT = config['DEFAULT']['RIGHT']
+UP = config['DEFAULT']['UP']
+DOWN = config['DEFAULT']['DOWN']
+BTN_A = config['DEFAULT']['BTN_A']
+BTN_B = config['DEFAULT']['BTN_B']
+BTN_X = config['DEFAULT']['BTN_X']
+BTN_Y = config['DEFAULT']['BTN_Y']
+BTN_L = config['DEFAULT']['BTN_L']
+BTN_R = config['DEFAULT']['BTN_R']
+SELECT = config['DEFAULT']['SELECT']
+START = config['DEFAULT']['START']
+#
