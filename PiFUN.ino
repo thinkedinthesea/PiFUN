b0304203d5e476e580022421cb28b7a6573c6879
Devid <45665085+thinkedinthesea@users.noreply.github.com>
2020-01-12 14:18:11 +0100

    Update PiFUN.ino

--- PiFUN.ino
+++ PiFUN.ino
@@ -14,38 +14,38 @@
    -------------
    PIN  ARDUINOPIN   TYPE             ALIAS        RPI-PIN        DESCRIPTION
    --------------------------------------------------------------------------------------------------------------------------
-   11   2            OUTPUT           JRIGHT       BCM 22         movimento destro attivato dallo stick. Attivo basso
-   10   3            OUTPUT           JLEFT        BCM 27         movimento sinistro attivato dallo stick. Attivo basso
-   9    4            OUTPUT           JDOWN        BCM 17         movimento giu attivato dallo stick. Attivo basso
-   8    5            OUTPUT           JUP          BCM 4          movimento su attivato dallo stick. Attivo basso
-   7    6            ANALOG-IN        XINPUT       ######         ingresso analogico asse X dello stick
-   6    7            ANALOG-IN        YINPUT       ######         ingresso analogico asse Y dello stick
+   11   2            OUTPUT           JRIGHT       BCM 22         right stick. Active low
+   10   3            OUTPUT           JLEFT        BCM 27         left stick. Active low
+   9    4            OUTPUT           JDOWN        BCM 17         down stick. Active low
+   8    5            OUTPUT           JUP          BCM 4          up stick. Active low
+   7    6            ANALOG-IN        XINPUT       ######         x analog input
+   6    7            ANALOG-IN        YINPUT       ######         y analog input
    --------------------------------------------------------------------------------------------------------------------------
 */
 //
-//definiamo le costanti
+//declarations
 const int JRIGHT = 2;
 const int JLEFT = 3;
 const int JDOWN = 4;
 const int JUP = 5;
-//ingressi analogici dello stick
+//analog stick input
 int XINPUT = A6;
 int YINPUT = A7;
-//variabili per memorizzare il valore di XINPUT ed YINPUT
+//store the value of XINPUT and YINPUT
 int Xval = 0;
 int Yval = 0;
-//soglie di isteresi dello stick
+//thresholds for stick
 int HL = 460;
 int HH = 600;
 
 void setup() {
-  //dichiariamo i pin
+  //
   pinMode (JRIGHT, OUTPUT);
   pinMode (JLEFT, OUTPUT);
   pinMode (JDOWN, OUTPUT);
   pinMode (JUP, OUTPUT);
   //
-  //comandiamo gli output dello stick a riposo alti
+  //all output high
   digitalWrite(JRIGHT, HIGH);
   digitalWrite(JLEFT, HIGH);
   digitalWrite(JUP, HIGH);
@@ -57,7 +57,7 @@ void loop() {
   delay(100);
   Yval = analogRead(YINPUT);
   //delay(100);
-  //li confrontiamo e comandiamo gli output
+  //compare with thresholds values and activate outputs
   if ((Xval > HL) && (Xval < HH)) {
     digitalWrite(JLEFT, HIGH);
     digitalWrite(JRIGHT, HIGH);
